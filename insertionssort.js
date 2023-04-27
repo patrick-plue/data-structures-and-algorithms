b5697012e111f28151f159086cea4387d707b1c6
patrick-plue <patrick.pluequett@wbscodingschool.com>
2023-04-27 17:30:08 +0200

    add insertion sort

---
+++ insertionssort.js
@@ -0,0 +1,12 @@
+function insertionSort(nums) {
+  for (let i = 1; i < nums.length; i++) {
+    let numberToInsert = nums[i];
+    let j;
+
+    for (j = i - 1; nums[j] > numberToInsert && j >= 0; j--) {
+      nums[j + 1] = nums[j];
+    }
+    nums[j + 1] = numberToInsert;
+  }
+  return nums;
+}
