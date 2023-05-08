8d143f92baccfbc002e7cab4628b040552bae03c
patrick-plue <patrick.pluequett@wbscodingschool.com>
2023-05-08 16:37:25 +0200

    differentiate linked lists, add exercises

--- datastructures/LinkedList.js
+++ datastructures/LinkedList.js
@@ -1,4 +1,4 @@
-const Node = require("./Node");
+const { SimpleNode, Node } = require("./Node");
 
 class LinkedList {
   constructor() {
@@ -6,7 +6,7 @@ class LinkedList {
   }
 
   addToHead(data) {
-    const newHead = new Node(data);
+    const newHead = new SimpleNode(data);
     const currentHead = this.head;
     this.head = newHead;
     if (currentHead) {
@@ -17,7 +17,7 @@ class LinkedList {
   addToTail(data) {
     let tail = this.head;
     if (!tail) {
-      this.head = new Node(data);
+      this.head = new SimpleNode(data);
     } else {
       while (tail.getNextNode() !== null) {
         tail = tail.getNextNode();
@@ -47,4 +47,105 @@ class LinkedList {
   }
 }
 
-module.exports = LinkedList;
+class DoublyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+
+  addToHead(data) {
+    const newHead = new Node(data);
+    const currentHead = this.head;
+    if (currentHead) {
+      currentHead.setPreviousNode(newHead);
+      newHead.setNextNode(currentHead);
+    }
+    this.head = newHead;
+    if (!this.tail) {
+      this.tail = newHead;
+    }
+  }
+
+  addToTail(data) {
+    const newTail = new Node(data);
+    const currentTail = this.tail;
+    if (currentTail) {
+      currentTail.setNextNode(newTail);
+      newTail.setPreviousNode(currentTail);
+    }
+    this.tail = newTail;
+    if (!this.head) {
+      this.head = newTail;
+    }
+  }
+
+  removeHead() {
+    const removedHead = this.head;
+    if (!removedHead) {
+      return;
+    }
+    this.head = removedHead.getNextNode();
+    if (this.head) {
+      this.head.setPreviousNode(null);
+    }
+    if (removedHead === this.tail) {
+      this.removeTail();
+    }
+    return removedHead.data;
+  }
+
+  removeTail() {
+    const removedTail = this.tail;
+    if (!removedTail) {
+      return;
+    }
+    this.tail = removedTail.getPreviousNode();
+    if (this.tail) {
+      this.tail.setNextNode(null);
+    }
+    if (removedTail === this.head) {
+      this.removeHead();
+    }
+    return removedTail.data;
+  }
+
+  removeByData(data) {
+    let nodeToRemove;
+    let currentNode = this.head;
+    while (currentNode !== null) {
+      if (currentNode.data === data) {
+        nodeToRemove = currentNode;
+        break;
+      }
+      currentNode = currentNode.getNextNode();
+    }
+    if (!nodeToRemove) {
+      return null;
+    }
+    // Continue your .removeByData() method below:
+    if (nodeToRemove === this.head) {
+      this.removeHead();
+    } else if (nodeToRemove === this.tail) {
+      this.removeTail();
+    } else {
+      const nextNode = nodeToRemove.getNextNode();
+      const previousNode = nodeToRemove.getPreviousNode();
+      nextNode.setPreviousNode(previousNode);
+      previousNode.setNextNode(nextNode);
+    }
+    return nodeToRemove;
+  }
+
+  printList() {
+    let currentNode = this.head;
+    let output = "<head> ";
+    while (currentNode !== null) {
+      output += currentNode.data + " ";
+      currentNode = currentNode.getNextNode();
+    }
+    output += "<tail>";
+    console.log(output);
+  }
+}
+
+module.exports = { DoublyLinkedList, LinkedList };
--- datastructures/node.js
+++ datastructures/node.js
@@ -1,3 +1,41 @@
+// Node class for simple linked list
+
+class SimpleNode {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+
+  setNextNode(node) {
+    if (node instanceof SimpleNode || node === null) {
+      this.next = node;
+    } else {
+      throw new Error("Next node must be a member of the Node class.");
+    }
+  }
+
+  getNextNode() {
+    return this.next;
+  }
+}
+
+const strawberryNode = new SimpleNode("Berry Tasty");
+const vanillaNode = new SimpleNode("Vanilla");
+const coconutNode = new SimpleNode("Coconuts for Coconut");
+
+vanillaNode.setNextNode(strawberryNode);
+strawberryNode.setNextNode(coconutNode);
+
+let currentNode = new SimpleNode();
+currentNode.setNextNode(vanillaNode);
+
+while (currentNode !== null) {
+  console.log(currentNode.data);
+  currentNode = currentNode.getNextNode();
+}
+
+// Node class for double linked list
+
 class Node {
   constructor(data) {
     this.data = data;
@@ -12,24 +50,21 @@ class Node {
     }
   }
 
+  setPreviousNode(node) {
+    if (node instanceof Node || node === null) {
+      this.previous = node;
+    } else {
+      throw new Error("Previous node must ve a member of the Node class");
+    }
+  }
+
   getNextNode() {
     return this.next;
   }
+
+  getPreviousNode() {
+    return this.previous;
+  }
 }
 
-const strawberryNode = new Node("Berry Tasty");
-const vanillaNode = new Node("Vanilla");
-const coconutNode = new Node("Coconuts for Coconut");
-
-vanillaNode.setNextNode(strawberryNode);
-strawberryNode.setNextNode(coconutNode);
-
-let currentNode = new Node();
-currentNode.setNextNode(vanillaNode);
-
-while (currentNode !== null) {
-  console.log(currentNode.data);
-  currentNode = currentNode.getNextNode();
-}
-
-module.exports = Node;
+module.exports = { Node, SimpleNode };
