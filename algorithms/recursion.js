1994971fd90d4c01568ccc8a3093f445ee44008e
patrick-plue <patrick.pluequett@wbscodingschool.com>
2023-08-30 18:16:24 +0200

    add recursion

--- algorithms/recursion.js
+++ algorithms/recursion.js
@@ -1,3 +1,6 @@
+const { SimpleNode } = require("../datastructures/Node.js");
+const { LinkedList } = require("../datastructures/LinkedList.js");
+
 function nestedAdd(array) {
   let sum = 0;
   for (let i = 0; i < array.length; i++) {
@@ -10,5 +13,33 @@ function nestedAdd(array) {
   }
   return sum;
 }
+
+const recursiveFactorial = (n) => {
+  if (n === 0) {
+    return 1;
+  } else if (n > 0) {
+    return n * recursiveFactorial(n - 1);
+  }
+};
+
+const iterativeFactorial = (n) => {
+  result = 1;
+  while (n > 0) {
+    result *= n;
+    n -= 1;
+  }
+  return result;
+};
+
+const myList = new LinkedList();
+
+myList.addToHead("Node 1");
+myList.addToHead("Node 2");
+myList.addToHead("Node 3");
+myList.addToHead("Node 4");
+
+// Add checkpoint 2 code below:
+const myNodeRecursive = myList.findNodeIteratively("Node 2");
+console.log(myNodeRecursive);
 
 module.exports = { nestedAdd };
